02643154247c0768e2e1c0bf19bcba4b90d00ff9
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-08-09 16:28:42 -0400

    Added code to replace qwith right child if node val is too small

--- BinaryTree-RangeFilter.cpp
+++ BinaryTree-RangeFilter.cpp
@@ -40,8 +40,13 @@ void BFS(TreeNode* root) {
 
 TreeNode* removeOutsideRange(TreeNode* root, int minVal, int maxVal) {
     if (!root) return nullptr; // Base case: null node
-    
+
     // Recursively process left and right subtrees    
     root->left = removeOutsideRange(root->left, minVal, maxVal);
     root->right = removeOutsideRange(root->right, minVal, maxVal);
+
+    if (root->val < minVal) {
+        TreeNode* rightChild = root->right;
+        delete root; 
+        return rightChild;
 }
