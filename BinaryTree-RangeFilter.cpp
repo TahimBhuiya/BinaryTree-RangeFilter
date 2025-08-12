bc7a04abf704bad660305e6daccd024e64c9eda6
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-08-12 15:49:58 -0400

    Initialized binary tree manually

--- BinaryTree-RangeFilter.cpp
+++ BinaryTree-RangeFilter.cpp
@@ -93,4 +93,15 @@ void readValidRange(int& minVal, int& maxVal) {
 }}
 
 
-int main() {}
+int main() {
+    TreeNode* root = new TreeNode(8);
+    root->left = new TreeNode(5);
+    root->right = new TreeNode(11);
+    root->left->left = new TreeNode(2);
+    root->left->right = new TreeNode(7);
+    root->right->left = new TreeNode(9);
+    root->right->right = new TreeNode(12);
+    root->left->right->left = new TreeNode(6);
+    root->right->left->right = new TreeNode(10);
+    root->right->right->right = new TreeNode(13);
+}
